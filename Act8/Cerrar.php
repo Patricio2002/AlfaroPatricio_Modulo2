a9713cd3a851715fe227f6ad70c4fe63f9973050
pato <patricio.ad26301@gmail.com>
2021-05-15 23:18:30 -0500

    correccion rápida

--- Act8/Cerrar.php
+++ Act8/Cerrar.php
@@ -1,5 +1,9 @@
 <?php
+    session_start();
+    include("./config.php");
+    $conexion=connectDB();
     if(isset($_POST["CERRAR"])){
+        $borrar="DELETE FROM alumno WHERE Ncuenta = $_SESSION[Cuenta];";
         session_unset();
         session_destroy();
     }
